1f5336b36f133f537da9c6ef022d53ae6173452a
nitinkrishnah <kittynitin2000@gmail.com>
2024-04-29 21:42:20 +0530

    Two sum

---
+++ Arrays/TwoSum.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/two-sum/
+
+# using dictionary(most efficient)
+# declare an empty dictionary
+# loop through the array
+#     for every number in array
+#         calculate the difference
+#         check if difference is already in dictionary
+#             if yes return the indices
+#             else add the number as key and index as Value
+
+
+def twoSum(nums, target):
+        nmap = {}
+        for i in range(len(nums)):
+            num=nums[i]
+            diff = target - num
+            if diff in nmap:
+                return [i,nmap[diff]]
+            nmap[num]=i
+        return []
+
+result = twoSum([3,2,4],6)
+print(result)
